ebe4ea8aaa96994ac633af74fe437b35468f1dea
Caleb Hicks <calebhicks@gmail.com>
2015-04-05 21:52:30 -0600

    Initial Commit

---
+++ ZipCodes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZipCodes
+//
+//  Created by Caleb Hicks on 4/5/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZipCodes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZipCodes
+//
+//  Created by Caleb Hicks on 4/5/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
